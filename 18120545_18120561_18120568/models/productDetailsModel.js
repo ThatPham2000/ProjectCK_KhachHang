715cb2802e29ed24cb623978ba9d25de287f92c2
NhatTai20 <ntai20111@gmail.com>
2020-12-01 15:37:04 +0700

    fix exports module

--- 18120545_18120561_18120568/models/productDetailsModel.js
+++ 18120545_18120561_18120568/models/productDetailsModel.js
@@ -99,7 +99,7 @@ const ProductsLaptopsChema = new mongoose.Schema({ //schema of item displayed on
     },
 });
 
-module.exports = mongoose.model('ProductLaptop', ProductsChema);//accessing a model
+module.exports = mongoose.model('ProductLaptop', ProductsLaptopsChema);//accessing a model
 
 const ProductsPCsChema = new mongoose.Schema({ //schema of item displayed on fron page
     pathImages: {
@@ -184,7 +184,7 @@ const ProductsPCsChema = new mongoose.Schema({ //schema of item displayed on fro
     },
 });
 
-module.exports = mongoose.model('ProductPC', ProductsChema);//accessing a model
+module.exports = mongoose.model('ProductPC', ProductsPCsChema);//accessing a model
 
 const ProductsMonitorsChema = new mongoose.Schema({ //schema of item displayed on fron page
     pathImages: {
@@ -269,7 +269,7 @@ const ProductsMonitorsChema = new mongoose.Schema({ //schema of item displayed o
     },
 });
 
-module.exports = mongoose.model('ProductAirConditioner', ProductsChema);//accessing a model
+module.exports = mongoose.model('ProductAirConditioner', ProductsMonitorsChema);//accessing a model
 
 const ProductsVGAChema = new mongoose.Schema({ //schema of item displayed on fron page
     pathImages: {
@@ -350,7 +350,7 @@ const ProductsVGAChema = new mongoose.Schema({ //schema of item displayed on fro
     },
 });
 
-module.exports = mongoose.model('ProductVGAs', ProductsChema);//accessing a model
+module.exports = mongoose.model('ProductVGAs', ProductsVGAChema);//accessing a model
 
 const ProductsCamerasChema = new mongoose.Schema({ //schema of item displayed on fron page
     pathImages: {
@@ -423,4 +423,4 @@ const ProductsCamerasChema = new mongoose.Schema({ //schema of item displayed on
     },
 });
 
-module.exports = mongoose.model('ProductCamera', ProductsChema);//accessing a model
+module.exports = mongoose.model('ProductCamera', ProductsCamerasChema);//accessing a model
